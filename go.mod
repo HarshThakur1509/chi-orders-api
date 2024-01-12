58e4cc56937863ef2330be562dd199a79f86efdb
khoprutu <harshthakur1592@gmail.com>
2024-01-12 18:24:54 +0530

    added handler

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/HarshThakur1509/chi-orders-api
 
 go 1.21.6
 
-require github.com/go-chi/chi/v5 v5.0.11 // indirect
+require github.com/go-chi/chi/v5 v5.0.11
--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
+
+	"github.com/HarshThakur1509/chi-orders-api/application"
 )
 
 func main() {
-	fmt.Print("hello")
+	app := application.New()
+
+	err := app.Start(context.TODO())
+	if err != nil {
+		fmt.Println("failed to start the app:", err)
+	}
 }
